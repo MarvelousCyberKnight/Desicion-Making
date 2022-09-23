0f75e49485f56ef942d97e4c426b826be9cebac3
SIDDARDA GOWTHAM <85551731+SIDDARADA@users.noreply.github.com>
2022-09-23 20:48:37 +0530

    Create budget.py

---
+++ budget.py
@@ -0,0 +1,16 @@
+A=float(input())
+B=float(input())
+C=float(input())
+D=float(input())
+E=float(input())
+tb=15000
+cb=A*350.34
+cv=B*230.90
+cau=C*90.55
+car=D*125.30
+cl=E*180.90
+rb=cb+cv+cau+car+cl
+if rb<tb:
+    print("yes")
+else:
+    print("no")
