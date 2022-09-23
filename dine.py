851d10468351f5f00147bd04e06c9ce1fce72a1e
SIDDARDA GOWTHAM <85551731+SIDDARADA@users.noreply.github.com>
2022-09-23 20:48:58 +0530

    Create dine.py

---
+++ dine.py
@@ -0,0 +1,12 @@
+entrance=input()
+rail=int(input())
+if entrance == "front":
+    if rail == 1:
+        print("Left Handed")
+    else:
+        print("Right Handed")
+else:
+    if rail == 1:
+        print("Right Handed")
+    else:
+        print("Left Handed")
